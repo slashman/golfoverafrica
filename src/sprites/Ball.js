990cd43f4a4bbf3f2a784638de09c999b0fa8e3f
slash <slash@slashware.net>
2018-04-21 13:28:16 -0500

    Apply effects to ball animation, calculate time based on force

--- src/sprites/Ball.js
+++ src/sprites/Ball.js
@@ -7,20 +7,18 @@ export default class extends Phaser.Sprite {
     this.anchor.setTo(0.5)
   }
 
-  shoot (force, direction, rise) {
-    console.log("Shoot", force, direction, rise);
-    force = force * 20
-    const endLocation = { // TODO: Include rise on the mix
-      x: this.x + Math.cos(direction) * force,
-      y: this.y + Math.sin(direction) * force
+  shoot (force, direction) {
+    const realForce = force * 20
+    const endLocation = {
+      x: this.x + Math.cos(direction) * realForce,
+      y: this.y + Math.sin(direction) * realForce
     }
-    console.log("startLocation", { x: this.x, y: this.y });
-    console.log("endLocation", endLocation);
-    // TODO: Calculate time based on rise
-    const time = 2000
-    // TODO: Tween ball scale
-    // TODO: Add easing
-    this.game.add.tween(this).to({ x: endLocation.x, y: endLocation.y }, time, Phaser.Easing.None, true);
+    const time = 2000 - (10 - force) * 100
+    if (force > 2) {
+      const toScale = (1 / 10) * force
+      this.game.add.tween(this.scale).to({ x: toScale, y: toScale }, time / 2, Phaser.Easing.Sinusoidal.Out, true, 0, 0, true)
+    }
+    this.game.add.tween(this).to({ x: endLocation.x, y: endLocation.y }, time, Phaser.Easing.Sinusoidal.Out, true)
     return Time.wait(time)
   }
 }
